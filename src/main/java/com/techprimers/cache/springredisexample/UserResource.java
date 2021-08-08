d737abfae7f8519f9fcc912805954bc4d1d0f7df
Vuong <vuong@11.com>
2021-08-08 16:29:49 +0700

    redis

---
+++ src/main/java/com/techprimers/cache/springredisexample/UserResource.java
@@ -0,0 +1,43 @@
+package com.techprimers.cache.springredisexample;
+
+
+import com.techprimers.cache.springredisexample.model.User;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/rest/user")
+public class UserResource {
+
+    private UserRepository userRepository;
+
+    public UserResource(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+    @GetMapping("/add/{id}/{name}")
+
+    public User add(@PathVariable("id") final String id,
+                    @PathVariable("name") final String name) {
+        userRepository.save(new User(id, name, 20000L));
+        return userRepository.findById(id);
+    }
+
+    @GetMapping("/update/{id}/{name}")
+    public User update(@PathVariable("id") final String id,
+                       @PathVariable("name") final String name) {
+        userRepository.update(new User(id, name, 1000L));
+        return userRepository.findById(id);
+    }
+
+    @GetMapping("/delete/{id}")
+    public Map<String, User> delete(@PathVariable("id") final String id) {
+        userRepository.delete(id);
+        return all();
+    }
+
+    @GetMapping("/all")
+    public Map<String, User> all() {
+        return userRepository.findAll();
+    }
+}
